052e765fd2d4293f7c09bf4bd9c6d1821d6a9524
ChaseM765 <ChaseM765>
2024-06-28 13:42:43 -0700

    while and for loop added

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 def main():
     plist = []
-    ##################################################
-    # Comlete your code here
-    ##################################################
+    begin = int(input('Enter value greater than 1: '))
+    end = int(input('Enter value greater than previous: '))
+    while (begin < 1 or begin > end):
+        begin = int(input('The number must be more than 1 and less than other entry: '))
+        print ('Other entry:', end)
+
+    for num in range(begin , end):
+        if num > 1:
+            for i in range(2,num):
+                if (num % i) == 0:
+                    break
+            else:
+                plist.append(num)
+    print(*plist)
 
     return plist
 
